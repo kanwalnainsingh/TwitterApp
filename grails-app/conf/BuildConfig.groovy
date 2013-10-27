3ce0e1c00dcb62a5fbc9749c50751e51e7f18aea
Manish Bharti <manish.bharti1988@gmail.com>
2013-10-27 22:13:41 +0530

    Spring security downgrade

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
         runtime ':twitter-bootstrap:2.3.2'
-        compile ":spring-security-core:2.0-RC2"
+        compile ":spring-security-core:1.2.7.3"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
